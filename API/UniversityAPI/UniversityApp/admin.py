1319a920d43994021d50595a00dca09be74aa87b
Mehmet Gulsen <mehmetgulsen95@hotmail.com>
2017-04-17 17:14:16 +0300

    more model fields and __str__

--- API/UniversityAPI/UniversityApp/admin.py
+++ API/UniversityAPI/UniversityApp/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from .models import Student
+
+admin.site.register(Student)
--- API/UniversityAPI/UniversityApp/models.py
+++ API/UniversityAPI/UniversityApp/models.py
@@ -4,3 +4,8 @@ class Student(models.Model):
 	name = models.CharField(max_length=50)
 	number = models.IntegerField()
 	gpa = models.FloatField()
+	department = models.CharField(max_length=50, default='none')
+	university = models.CharField(max_length=50, default='none')
+
+	def __str__(self):
+		return self.name
