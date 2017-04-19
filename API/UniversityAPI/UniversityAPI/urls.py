b77c611b0988f12404063c95c98e3690b8a6065b
mustafafeyzioglu <mstafafeyzioglu@gmail.com>
2017-04-19 13:01:21 +0300

    URL patterns are added

--- API/UniversityAPI/UniversityAPI/urls.py
+++ API/UniversityAPI/UniversityAPI/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+from UniversityApp import views
+
 
 urlpatterns = [
     # Examples:
@@ -7,4 +10,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^students/', views.StudentList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
