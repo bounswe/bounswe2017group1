6a3f4bab2fa62211a45a7cd9cc2848f5e84ba51d
Hakan Şirin <hakan.sirin.95@gmail.com>
2017-10-23 22:30:45 +0300

    old unused project folder deleted

--- project/frontend/node_modules/react-toolbox/lib/utils/is-browser.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = isBrowser;
-function isBrowser() {
-  return typeof window !== 'undefined' && window.document;
-}
