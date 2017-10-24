1593843ceb279c1b2f341f53bdb0e2b05a490243
mehmetsefa <mehmetsefa2644@gmail.com>
2017-10-24 15:32:47 +0300

    added intent to open signup while opening

--- android/app/src/main/java/com/boungroup1/androidculturemania/MainActivity.java
+++ android/app/src/main/java/com/boungroup1/androidculturemania/MainActivity.java
@@ -1,5 +1,6 @@
 package com.boungroup1.androidculturemania;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, SingUpActivity.class);
+        startActivity(intent);
     }
 }
