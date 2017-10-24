a9cfd90c67171a858f12effb09ccfb65a6252803
mehmetsefa <mehmetsefa2644@gmail.com>
2017-10-24 14:55:54 +0300

    working on api signup post request, there are several issues, I will fix them. This is an intermediate commit

--- android/app/src/main/java/com/boungroup1/androidculturemania/LoginActivity.java
+++ android/app/src/main/java/com/boungroup1/androidculturemania/LoginActivity.java
@@ -3,6 +3,11 @@ package com.boungroup1.androidculturemania;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 /**
  * Created by mustafa on 10/20/17.
  */
@@ -13,6 +18,23 @@ public class LoginActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Retrofit retrofit = ApiClient.getApiClient();
+
+        ApiInterface apiInterface = retrofit.create(ApiInterface.class);
+        Call<User> call = apiInterface.signIn();
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
+            }
+        });
     }
 
 }
