65afa17c9dd4ef34f9bb509c9615b4796e6c3dfe
Hakan Şirin <hakan.sirin.95@gmail.com>
2017-10-28 22:48:33 +0300

    comment api controller implemented, redundant profile field in api calls is removed, auto creation date control is added to models

--- web/api/model/comment.py
+++ web/api/model/comment.py
@@ -5,8 +5,12 @@ from profile import Profile
 from heritage import Heritage
 
 class Comment(models.Model):
-    text = models.TextField();
+    text = models.TextField()
     heritage = models.ForeignKey(Heritage)
     creator = models.ForeignKey(Profile)
-    #parent = models.ForeignKey(Comment)
-    date = models.DateTimeField()
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
