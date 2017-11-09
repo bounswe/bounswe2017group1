4c8ddc3795c63f1b8c727f930853af95cd5cb473
Ceyhun <ceyhunuzngl@gmail.com>
2017-11-09 18:59:42 +0300

    Bug Fix: api/model/tag.py ; remove "unique=True"

--- web/api/model/tag.py
+++ web/api/model/tag.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(unique=True, max_length=25)
+    name = models.CharField(max_length=25)
     category = models.CharField(max_length=25, blank=True, null=True)
 
 
