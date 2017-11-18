c9ed8e64ed2ca8503bd52a7b0943b4705c71cc64
basriyilmazturk <basriyilmazturk@gmail.com>
2017-11-18 19:20:10 +0300

    amazon frontend changes

--- web/frontend/client/modules/appConstants.js
+++ web/frontend/client/modules/appConstants.js
@@ -1,5 +1,5 @@
 const appConstants = {
-	baseUrl:'http://ec2-18-196-2-56.eu-central-1.compute.amazonaws.com',
+	baseUrl:'http://ec2-18-196-2-56.eu-central-1.compute.amazonaws.com:3000',
 	deneme:'123'
 }
 
