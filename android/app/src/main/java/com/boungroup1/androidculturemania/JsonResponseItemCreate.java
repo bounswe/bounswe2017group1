22e40f8984386bb48947b6c5940207377e32d58d
tahabayi <tahabayi@gmail.com>
2017-12-05 20:56:47 +0300

    Image upload implemented

--- android/app/src/main/java/com/boungroup1/androidculturemania/JsonResponseItemCreate.java
+++ android/app/src/main/java/com/boungroup1/androidculturemania/JsonResponseItemCreate.java
@@ -1,8 +1,24 @@
 package com.boungroup1.androidculturemania;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by user on 12/11/2017.
  */
 
 public class JsonResponseItemCreate {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
 }
