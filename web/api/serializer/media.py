c32e439b8093c296d2971b33d6a928601899e241
Mehmet Gulsen <mehmetgulsen95@hotmail.com>
2017-12-24 15:41:26 +0300

    Video support added

--- web/api/serializer/media.py
+++ web/api/serializer/media.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 
 class MediaSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True)
+    image = serializers.ImageField(max_length=None, use_url=True, allow_null=True, allow_empty_file=True)
 
     class Meta:
         model = Media
