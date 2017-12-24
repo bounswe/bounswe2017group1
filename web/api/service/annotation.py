a8205ed2f0fa196c834401d7480c94c174a957b8
furkan.ilisu <frkn.ilisu@gmail.com>
2017-12-24 23:29:14 +0300

    post annotation to local

--- web/api/service/annotation.py
+++ web/api/service/annotation.py
@@ -11,5 +11,5 @@ def post_annotation(body, target):
 
     header = {"Content-Type": "application/ld+json"}
 
-    r = requests.post("ec2-18-196-2-56.eu-central-1.compute.amazonaws.com:8000/annotations/", data=data, header=header)
+    r = requests.post("127.0.0.1:8000/annotations/", data=data, header=header)
     return r.status_code
