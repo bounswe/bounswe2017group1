aa4d6638f2f3ccf68f059f4e6167c55876f3b1c5
Hakan Şirin <hakan.sirin.95@gmail.com>
2018-01-02 12:56:33 +0300

    urls comment change

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,3 +1,8 @@
+"""api URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+
+"""
 from django.conf.urls import url, include
 from django.contrib import admin
 
